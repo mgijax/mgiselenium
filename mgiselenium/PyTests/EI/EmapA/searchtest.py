2b4aee9ffcd5070b37f06d902e55b4ea00458c4b
Kevin Stone <kevinrstone@gmail.com>
2016-02-11 12:55:13 -0500

    changed assert methods

--- mgiselenium/PyTests/EI/EmapA/searchtest.py
+++ mgiselenium/PyTests/EI/EmapA/searchtest.py
@@ -20,25 +20,27 @@ class searchTest(unittest.TestCase):
         searchbox.send_keys("brain")
         searchbox.send_keys(Keys.RETURN)
         term_result = self.driver.find_element_by_id("emapTermArea")
-        assert term_result.text() == 'brain TS17-28'
+        self.assertIn('brain TS17-28', term_result.text)
         
     def testSynonymSearch(self):#tests that a synonym term search works
         searchbox = self.driver.find_element_by_id("termSearch")
         searchbox.send_keys("myocardium")
         searchbox.send_keys(Keys.RETURN)
+        assert False
         #assert "cardiac muscle tissue TS12-28" in self.driver.find_element_by_id("emapTermArea").text()        
 
     def testWildcardSearch(self):#tests that a wildcard term search works
         searchbox = self.driver.find_element_by_id("termSearch")
         searchbox.send_keys("%tectum")
         searchbox.send_keys(Keys.RETURN)
-        assert "pretectum TS22-28 tectum TS22-28" in self.driver.find_element_by_id("emapTermArea").text
+        self.assertIn("pretectum TS22-28 tectum TS22-28" in self.driver.find_element_by_id("emapTermArea").text)
         
     def testtermwithcommaSearch(self):#tests that a term with a comma search works
         searchbox = self.driver.find_element_by_id("termSearch")
         searchbox.send_keys("female,%")
         searchbox.send_keys(Keys.RETURN)
         result = self.driver.find_element_by_id("emapTermArea")
+        assert False
         #assert result.text == 
             
     def tearDown(self):
