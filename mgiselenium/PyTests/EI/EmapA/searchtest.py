70a4b2de9d6e77e42007d848b885619c49425177
Kevin Stone <kevinrstone@gmail.com>
2016-02-15 11:27:27 -0500

    1) Refactored code style. 2) made helper method getSearchTextAsList() 3) Corrected tests for latest version of emapaBrowser

--- mgiselenium/PyTests/EI/EmapA/searchtest.py
+++ mgiselenium/PyTests/EI/EmapA/searchtest.py
@@ -8,53 +8,94 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class searchTest(unittest.TestCase):
+# Constants
+BROWSER_URL = "http://scrumdog.informatics.jax.org/pwi/edit/emapBrowser"
+# TODO(kstone): move browser urls to a config file
+#BROWSER_URL = "http://mlg-balrog.jax.org:7000/pwi/edit/emapBrowser"
 
+# Tests
+
+class SearchTest(unittest.TestCase):
+    """
+    Test EMAPA browser search
+    """
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.get("http://scrumdog.informatics.jax.org/pwi/edit/emapBrowser")
+        self.driver.get(BROWSER_URL)
         self.driver.implicitly_wait(10)
 
-    def testSearch(self):#tests that a basic term search works
+    def testBasicSearch(self):
+        """
+        tests that a basic term search work
+        """
         searchbox = self.driver.find_element_by_id("termSearch")
         searchbox.send_keys("brain")
         searchbox.send_keys(Keys.RETURN)
         term_result = self.driver.find_element_by_id("termResultList")
         items = term_result.find_elements_by_tag_name("li")
-        for item in items:
-            text = item.text
-            print text
-        self.assertIn('brain TS17-28', term_result.text)
         
-    def testSynonymSearch(self):#tests that a synonym term search works
+        # add all li text to a list for "assertIn" test
+        searchTextItems = self.getSearchTextAsList(items)
+        
+        self.assertIn('brain TS17-28', searchTextItems)
+        
+    def testSynonymSearch(self):
+        """
+        tests that a synonym term search works
+        """
         searchbox = self.driver.find_element_by_id("termSearch")
         searchbox.send_keys("myocardium")
         searchbox.send_keys(Keys.RETURN)
         term_result = self.driver.find_element_by_id("emapTermArea")
         items = term_result.find_elements_by_tag_name("li")
-        for item in items:
-            text = item.text
-            print text
-        self.assertIn('cardiac muscle tissue TS12-28', term_result.text)        
+        
+        # add all li text to a list for "assertIn" test
+        searchTextItems = self.getSearchTextAsList(items)
+        
+        self.assertIn('cardiac muscle tissue TS12-28', searchTextItems)        
 
-    def testWildcardSearch(self):#tests that a wildcard term search works
+    def testWildcardSearch(self):
+        """
+        tests that a wildcard term search works
+        """
         searchbox = self.driver.find_element_by_id("termSearch")
         searchbox.send_keys("%tectum")
         searchbox.send_keys(Keys.RETURN)
         term_result = self.driver.find_element_by_id("termResultList")
         items = term_result.find_elements_by_tag_name("li")
-        for item in items:
-            text = item.text
-            print text
-        self.assertIn('pretectum TS22-28 tectum TS22-28', term_result.text)
         
-    def testtermwithcommaSearch(self):#tests that a term with a comma search works
+        # add all li text to a list for "assertIn" test
+        searchTextItems = self.getSearchTextAsList(items)
+        
+        self.assertIn('pretectum TS22-28', searchTextItems)
+        self.assertIn('tectum TS22-28', searchTextItems)
+        
+    def testTermWithCommaSearch(self):
+        """
+        tests that a term with a comma search works
+        """
         searchbox = self.driver.find_element_by_id("termSearch")
         searchbox.send_keys("female,%")
         searchbox.send_keys(Keys.RETURN)
         result = self.driver.find_element_by_id("emapTermArea")
         assert False
+        
+        
+    def getSearchTextAsList(self, liItems):
+        """
+        Take all found li tags in liItems
+            and return a list of all the text values
+            for each li tag
+        """
+        searchTextItems = []
+        for item in liItems:
+            text = item.text
+            searchTextItems.append(item.text)
+            
+        print "li text = %s" % searchTextItems
+        return searchTextItems
+        
         
             
     def tearDown(self):
