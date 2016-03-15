8c9414aa0d2a9accd496cdebd38e2c78b6082cfd
jeff campbell <jeff.campbell.jax.org>
2016-03-15 09:19:25 -0400

    updates to emapatreeview test

--- PyTests/EI/EmapA/emapatreeviewtest.py
+++ PyTests/EI/EmapA/emapatreeviewtest.py
@@ -4,7 +4,7 @@ This test verifies searching within the EmapA module
 @author: jeffc
 '''
 import unittest
-
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -45,6 +45,73 @@ class TreeViewTest(unittest.TestCase):
         searchTreeItems = self.getSearchTextAsList(items)
         
         self.assertEqual(["mouse", "body fluid or substance", "body region", "cavity or lining", "conceptus", "early embryo", "embryo", "extraembryonic component", "germ layer", "organ", "organ system", "tissue", "umbilical or vitelline vessel"], searchTreeItems)
+
+    def testSpecificStageTree(self):
+        """
+        tests that tree view changes to show just tree of term selected, test under construction
+        """
+        searchbox = self.driver.find_element_by_id("termSearch")
+        searchbox.send_keys("embryo")
+        searchbox.send_keys(Keys.RETURN)
+        treesort = self.driver.find_element_by_id("emapTree").find_element_by_class_name("mgitreeview")
+        items = treesort.find_elements_by_css_selector("mark")
+        
+        # add all li text to a list for "assertIn" test
+        searchTreeItems = self.getSearchTextAsList(items)
+        activestage = self.driver.find_element_by_id("stageList").find_element_by_class_name("stageSelector")
+        
+        self.assertEqual(["embryo","embryo"], searchTreeItems)
+        self.assertEqual(activestage.find_element_by_link_text("All"), "All", "incorrect stage selected")
+
+    def testdetailparent(self):
+        """
+        tests that term detail updates including valid parents, test under construction
+        """
+        searchbox = self.driver.find_element_by_id("termSearch")
+        searchbox.send_keys("mouse")
+        searchbox.send_keys(Keys.RETURN)
+        treesort = self.driver.find_element_by_id("emapTree").find_element_by_class_name("mgitreeview")
+        items = treesort.find_elements_by_css_selector(".node")
+        
+        # add all li text to a list for "assertIn" test
+        searchTreeItems = self.getSearchTextAsList(items)
+        
+        self.assertEqual(["mouse", "body fluid or substance", "body region", "cavity or lining", "conceptus", "early embryo", "embryo", "extraembryonic component", "germ layer", "organ", "organ system", "tissue", "umbilical or vitelline vessel"], searchTreeItems)
+
+    def testparentstage(self):
+        """
+        tests that if parent link is clicked remain in the current stage, test under construction
+        """
+        searchbox = self.driver.find_element_by_id("termSearch")
+        searchbox.send_keys("mouse")
+        searchbox.send_keys(Keys.RETURN)
+        treesort = self.driver.find_element_by_id("emapTree").find_element_by_class_name("mgitreeview")
+        items = treesort.find_elements_by_css_selector(".node")
+        
+        # add all li text to a list for "assertIn" test
+        searchTreeItems = self.getSearchTextAsList(items)
+        
+        self.assertEqual(["mouse", "body fluid or substance", "body region", "cavity or lining", "conceptus", "early embryo", "embryo", "extraembryonic component", "germ layer", "organ", "organ system", "tissue", "umbilical or vitelline vessel"], searchTreeItems)
+
+    def teststagelinks(self):
+        """
+        tests that all stage links exist in the term detail section and clicking them function correctly, test under construction
+        """
+        searchbox = self.driver.find_element_by_id("termSearch")
+        searchbox.send_keys("mouse")
+        searchbox.send_keys(Keys.RETURN)
+        treesort = self.driver.find_element_by_id("emapTree").find_element_by_class_name("mgitreeview")
+        items = treesort.find_elements_by_css_selector(".node")
+        time.sleep(5)
+        # add all li text to a list for "assertIn" test
+        searchTreeItems = self.getSearchTextAsList(items)
+        stagesort = self.driver.find_element_by_id(".stageList")
+        stageitems = stagesort.find_elements_by_class_name("stageSelector")
+        time.sleep(5)
+        # add all li text to a list for "assertIn" test
+        searchTreeItems = self.getSearchTextAsList(stageitems)
+        
+        self.assertEqual([stageitems.text, "All", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"], searchTreeItems)
 
     def getSearchTextAsList(self, liItems):
         """
