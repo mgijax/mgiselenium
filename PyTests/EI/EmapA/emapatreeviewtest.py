70e96bca5131afa99cd825863431bff5b1c8349e
jeff campbell <jeff.campbell.jax.org>
2016-03-15 15:02:49 -0400

    another emaptreeviewtest update

--- PyTests/EI/EmapA/emapatreeviewtest.py
+++ PyTests/EI/EmapA/emapatreeviewtest.py
@@ -59,16 +59,12 @@ class TreeViewTest(unittest.TestCase):
         # select specific stage
         stage20 = self.driver.find_element_by_id("stageList").find_element_by_link_text("20")
         stage20.click()
-        time.sleep(1)
-        self.assertEqual(stage20.text, "20")
-        
+        time.sleep(3)
         term_det = self.driver.find_element_by_id("termDetailContent")
         items = term_det.find_elements_by_tag_name("dd")
-        time.sleep(1)
         self.assertEqual(items[0].text, "embryo")
         self.assertEqual(items[1].text, "Theiler Stage 20")
         self.assertEqual(items[2].text, "EMAPS:1603920")
-        
         
         
     def testdetailparent(self):
@@ -76,16 +72,41 @@ class TreeViewTest(unittest.TestCase):
         tests that term detail updates including valid parents, test under construction
         """
         searchbox = self.driver.find_element_by_id("termSearch")
-        searchbox.send_keys("mouse")
+        searchbox.send_keys("cortical renal tubule")
         searchbox.send_keys(Keys.RETURN)
-        treesort = self.driver.find_element_by_id("emapTree").find_element_by_class_name("mgitreeview")
-        items = treesort.find_elements_by_css_selector(".node")
+        time.sleep(3)
+        term_det = self.driver.find_element_by_id("termDetailContent")
+        items = term_det.find_elements_by_tag_name("dd")
+        self.assertEqual(items[0].text, "cortical renal tubule")
+        self.assertEqual(items[1].text, "Theiler Stages 22-28")
+        self.assertEqual(items[2].text, "EMAPA:18976")
+        self.assertEqual(items[3].text, "kidney cortex tubule, renal cortex tubule")
+        self.assertEqual(items[4].text.split("\n"), ["part-of developing capillary loop stage nephron group","part-of early nephron","part-of late tubule","part-of mature nephron","part-of maturing nephron","part-of renal cortex","part-of stage IV immature nephron"])
         
-        # add all li text to a list for "assertIn" test
-        searchTreeItems = self.getSearchTextAsList(items)
+        stage24 = self.driver.find_element_by_id("stageList").find_element_by_link_text("24")
+        stage24.click()
+        time.sleep(3)
+        term_det = self.driver.find_element_by_id("termDetailContent")
+        items = term_det.find_elements_by_tag_name("dd")
+        self.assertEqual(items[0].text, "cortical renal tubule")
+        self.assertEqual(items[1].text, "Theiler Stage 24")
+        self.assertEqual(items[2].text, "EMAPS:1897624")
+        self.assertEqual(items[3].text, "kidney cortex tubule, renal cortex tubule")
+        self.assertEqual(items[4].text.split("\n"), ["part-of developing capillary loop stage nephron group","part-of early nephron","part-of late tubule","part-of maturing nephron","part-of renal cortex"])
         
-        self.assertEqual(["mouse", "body fluid or substance", "body region", "cavity or lining", "conceptus", "early embryo", "embryo", "extraembryonic component", "germ layer", "organ", "organ system", "tissue", "umbilical or vitelline vessel"], searchTreeItems)
-
+        stage22 = self.driver.find_element_by_id("stageList").find_element_by_link_text("22")
+        stage22.click()
+        time.sleep(3)
+        term_det = self.driver.find_element_by_id("termDetailContent")
+        items = term_det.find_elements_by_tag_name("dd")
+        self.assertEqual(items[0].text, "cortical renal tubule")
+        self.assertEqual(items[1].text, "Theiler Stage 22")
+        self.assertEqual(items[2].text, "EMAPS:1897622")
+        self.assertEqual(items[3].text, "kidney cortex tubule, renal cortex tubule")
+        self.assertEqual(items[4].text.split("\n"), ["part-of developing capillary loop stage nephron group","part-of early nephron","part-of late tubule","part-of maturing nephron","part-of renal cortex","part-of stage IV immature nephron"])
+        
+        
+        
     def testparentstage(self):
         """
         tests that if parent link is clicked remain in the current stage, test under construction
