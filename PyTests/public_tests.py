2ffd0f8db96a251abf71dac7e28e940b531cc121
test user <testuser@WDG-DUBLIN.jax.org>
2016-07-18 13:21:19 -0400

    import error

--- PyTests/public_tests.py
+++ PyTests/public_tests.py
@@ -5,7 +5,7 @@ import sys,os.path
 
 # adjust the path to find config
 sys.path.append(
-  os.path.join(os.path.dirname(__file__), '../../config',)
+  os.path.join(os.path.dirname(__file__), '../',)
 )
 import config
 
