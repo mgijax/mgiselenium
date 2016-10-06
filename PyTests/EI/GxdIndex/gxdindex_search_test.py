e6ec3ea2c94c61aec97508178f1f6ed48ad1385b
jeff campbell <jeff.campbell.jax.org>
2016-10-06 13:02:50 -0400

    updated gxd index tests

--- PyTests/EI/GxdIndex/gxdindex_search_test.py
+++ PyTests/EI/GxdIndex/gxdindex_search_test.py
@@ -42,198 +42,251 @@ class TestSearch(unittest.TestCase):
     def testJnumSearch(self):
         """
         @Status tests that a basic J number search works
-        @bug: test needs to capture user and dates
+        
         """
         driver = self.driver
         form = self.form
-        
         form.enter_value('jnumid', '173543')
         form.click_search()
-        
         #finds the citation field
         citation = form.get_value('citation')
-        
         print citation
         self.assertEqual(citation, 'Harper J, Proc Natl Acad Sci U S A 2011 Jun 28;108(26):10585-90')
-
         #finds the marker field
         marker_symbol = form.get_value('marker_symbol')
-        
         print marker_symbol
         self.assertEqual(marker_symbol, '1810065E05Rik')
-
         #finds the coded? field
         is_coded = form.get_value('is_coded')
-        
         print is_coded
         self.assertEqual(is_coded, 'false')
-
         #finds the priority field
         priority = form.get_selected_text('_priority_key')
-        
         print priority
         self.assertEqual(priority, 'High')
-        
-
         #finds the conditional mutants field
         conditional = form.get_selected_text('_conditionalmutants_key')
-        
         print conditional
         self.assertEqual(conditional, 'Conditional')
-
         #finds the created by field
-        created_user = driver.find_element_by_id('createdby_login')
-        
+        created_user = form.get_value('createdby_login')
         print created_user
-        self.assertEqual(created_user.text, 'jx')
-
-        
+        self.assertEqual(created_user, 'jx')
         #finds the modified by field
-        modified_user = driver.find_element_by_id('modifiedby_login')#.find_element_by_css_selector('td')
-        
+        modified_user = form.get_value('modifiedby_login')#.find_element_by_css_selector('td')
         print modified_user
-        self.assertEqual(modified_user.text, 'jx')
-        
+        self.assertEqual(modified_user, 'jx')
         #finds the created by date field
         created_date = form.get_value('creation_date')
-        
         print created_date
         self.assertEqual(created_date, '07/26/2011')
-        
         #finds the created by date field
         modified_date = form.get_value('modification_date')
-        
         print modified_date
         self.assertEqual(modified_date, '12/12/2011')
         
     def testInvalidJnumSearch(self):
         """
         @Status tests that an invalid J number search gives an error
-        @bug test needs to be written
+        
         """
         form = self.form
         form.enter_value('jnumid', "99999999")
         form.press_tab()
-        
         error = form.get_error_message()
-        
         self.assertEqual("No Reference for J Number=J:99999999", error)
-        
         
     def testMarkerSearch(self):
         """
-        @Status Tests that a marker symbol search works
-        @bug: test needs to capture user and dates
+        @Status Tests that a marker symbol search works, verifies the details of the first result listed
+        
         """
-        
         form = self.form
-        
         form.enter_value('marker_symbol', 'Pax6')
         form.click_search()
-        
         #finds the J number field
         jnumid = form.get_value('jnumid')
-        
         print jnumid
         self.assertEqual(jnumid, 'J:193837')
-        
-
         #finds the citation field
         citation = form.get_value('citation')
-        
         print citation
         self.assertEqual(citation, 'Abdelhamed ZA, Hum Mol Genet 2013 Apr 1;22(7):1358-72')
-
         #finds the coded? field
         is_coded = form.get_value('is_coded')
-        
         print is_coded
         self.assertEqual(is_coded, 'false')
- 
         #finds the priority field
         priority = form.get_selected_text('_priority_key')
-        
         print priority
         self.assertEqual(priority, 'Medium')
-        
- 
         #finds the conditional field
         conditional = form.get_selected_text('_conditionalmutants_key')
-        
         print conditional
         self.assertEqual(conditional, 'Not Applicable')
- 
+        #finds the created by field
+        created_user = form.get_value('createdby_login')
+        print created_user
+        self.assertEqual(created_user, 'terryh')
+        #finds the modified by field
+        modified_user = form.get_value('modifiedby_login')#.find_element_by_css_selector('td')
+        print modified_user
+        self.assertEqual(modified_user, 'terryh')
+        #finds the created by date field
+        created_date = form.get_value('creation_date')
+        print created_date
+        self.assertEqual(created_date, '03/26/2013')
+        #finds the created by date field
+        modified_date = form.get_value('modification_date')
+        print modified_date
+        self.assertEqual(modified_date, '03/26/2013')
         
-    def testWildcardSearch(self):
+    def testMrkWildcardSearch(self):
         """
         @Status tests that a wildcard search for a marker works
         @bug test needs to be written
         """
         form = self.form
         
-        form.enter_value('marker_symbol', 'Ad%re1')
+        form.enter_value('marker_symbol', 'unc5%')
+        form.enter_value('_priority_key', 'low')
         form.click_search()
         
         marker_symbol = form.get_value('marker_symbol')
-        self.assertEqual(marker_symbol, 'Adgre1')
+        self.assertEqual(marker_symbol, 'Unc5a')
+        #find the search results table
+        results_table = self.driver.find_element_by_id("resultsTable")
+        table = Table(results_table)
+        #Iterate and print the search results headers
+        header_cells = table.get_header_cells()
+        print iterate.getTextAsList(header_cells)
         
+        # print row 1
+        cells = table.get_row_cells(1)
+        print iterate.getTextAsList(cells)
+        #print column 1
+        symbols_cells = table.get_column_cells('Marker')
+        symbols = iterate.getTextAsList(symbols_cells)
+        self.assertEqual(symbols, ['Marker','Unc5a', 'Unc5a', 'Unc5b', 'Unc5b', 'Unc5b', 'Unc5c', 'Unc5c', 'Unc5c', 'Unc5d', 'Unc5d', 'Unc5d'])
+            
+    def testCitationWildcardSearch(self):
+        """
+        @Status tests that a wildcard search for a citation works
         
+        """
+        form = self.form
+        form.enter_value('citation', '%Blood 1991% ')
+        form.click_search()
+        marker_symbol = form.get_value('marker_symbol')
+        self.assertEqual(marker_symbol, 'Ptpn1')
+        #finds the citation field
+        citation = form.get_value('citation')
+        self.assertEqual(citation, 'Yi T, Blood 1991 Nov 1;78(9):2222-8')
+        #find the search results table
+        results_table = self.driver.find_element_by_id("resultsTable")
+        table = Table(results_table)
+        #Iterate and print the search results headers
+        header_cells = table.get_header_cells()
+        print iterate.getTextAsList(header_cells)
+        # print row 1, the table headers
+        cells = table.get_row_cells(1)
+        print iterate.getTextAsList(cells)
+        #print column 1, the marker symbols
+        symbols_cells = table.get_column_cells('Marker')
+        symbols = iterate.getTextAsList(symbols_cells)
+        self.assertEqual(symbols, ['Marker','Ptpn1', 'Ptpn12', 'Ptpn6', 'Ptpra', 'Ptprc', 'Ptpre', 'Ptprj'])
         
+    def testNotesWildcardSearch(self):
+        """
+        @Status tests that a wildcard search for a noteworks
+        
+        """
+        form = self.form
+        form.enter_value('comments', '%slot%')
+        form.press_tab()
+        form.enter_value('_priority_key', 'High')
+        form.press_tab()
+        form.enter_value('_conditionalmutants_key', '4834242')
+        form.press_tab()
+        form.enter_value('is_coded', 'true')
+        form.press_tab()
+        form.click_search()
+        note = form.get_value('comments')
+        self.assertEqual(note, 'Some of the northern data was obtained using slot blots.  Age of embryo at noon of plug day not specified in reference.')
+        #finds the citation field
+        citation = form.get_value('citation')
+        self.assertEqual(citation, 'Chianale J, Biochim Biophys Acta 1995 Dec 27;1264(3):369-76')
+        #find the search results table
+        results_table = self.driver.find_element_by_id("resultsTable")
+        table = Table(results_table)
+        #Iterate and print the search results headers
+        header_cells = table.get_header_cells()
+        print iterate.getTextAsList(header_cells)
+        # print row 1, the table headers
+        cells = table.get_row_cells(1)
+        print iterate.getTextAsList(cells)
+        #print column 1, the marker symbols
+        symbols_cells = table.get_column_cells('Marker')
+        symbols = iterate.getTextAsList(symbols_cells)
+        print symbols
+        self.assertEqual(symbols, ['Marker','Abcb1a', 'Sdc1', 'Sptb', 'Sptbn1'])
+                
     def testWithdrawnMrkSearch(self):
         """
         @Status tests that a search for a withdrawn marker gives an error
-        @bug test needs to be written
         """
         form = self.form
-        
         form.enter_value('marker_symbol', 'dw')
         form.press_tab()
-        
         error = form.get_error_message()
         # error message will display current symbol for dw
         self.assertIn("Pou1f1", error )
-        
         # marker entry should be cleared
         marker_symbol = form.get_value('marker_symbol')
         self.assertEqual(marker_symbol, '')
-        
         
     def testInvalidMrkSearch(self):
         """
         @Status tests that an error message is displayed when invalid marker symbol entered
-        @bug test needs to be written
+        
         """
         form = self.form
-        
         form.enter_value('marker_symbol', 'test12345')
         form.press_tab()
-        
         error = form.get_error_message()
-        self.assertEqual(error, "Invalid marker symbol: test12345")
-        
+        self.assertEqual(error, "Invalid marker symbol test12345")
         # marker entry should be cleared
         marker_symbol = form.get_value('marker_symbol')
         self.assertEqual(marker_symbol, '')
         
-        
     def testQTLErrorMsg(self):
         """
-        @Status tests that an error message is displayed when selecting a heritable phenotypic marker or QTL
-        @bug test needs to be written
+        @Status tests that an error message is displayed when selecting a QTL
+        
         """
         form = self.form
-        
         form.enter_value('marker_symbol', 'iba1')
         form.press_tab()
-        
         error = form.get_error_message()
         self.assertEqual(error, "You selected a QTL type marker: Iba1")
-        
         # marker should still be selected, even though error is displayed
         marker_symbol = form.get_value('marker_symbol')
         self.assertEqual(marker_symbol, 'Iba1')
         
+    def testHeritPhenoMrkErrorMsg(self):
+        """
+        @Status tests that an error(Warning) message is displayed when selecting a heritable phenotypic marker
         
+        """
+        form = self.form
+        form.enter_value('marker_symbol', 'act')
+        form.press_tab()
+        error = form.get_error_message()
+        self.assertEqual(error, "You selected a heritable phenotypic marker: act")
+        # marker should still be selected, even though warning error is displayed
+        marker_symbol = form.get_value('marker_symbol')
+        self.assertEqual(marker_symbol, 'act')
+            
     def testMultipleMrkSearch(self):
         """
         @Status tests that a multiple marker search works
@@ -259,27 +312,6 @@ class TestSearch(unittest.TestCase):
         marker_symbol = form.get_value('marker_symbol')
         self.assertEqual(marker_symbol, 'T')
     
-    
-    def testHeritablePhenotypicMarker(self):
-        """
-        @Status tests that a heritable phenotypic marker displays error/warning
-        @bug test needs to be written
-        """
-        form = self.form
-        
-        form.enter_value('marker_symbol', 't')
-        form.press_tab()
-        
-        form.enter_shortcut(Keys.ARROW_DOWN)
-        form.press_enter()
-        
-        error = form.get_error_message()
-        self.assertEqual(error, "You selected a heritable phenotypic marker: t")
-        
-        # marker t should be selected
-        marker_symbol = form.get_value('marker_symbol')
-        self.assertEqual(marker_symbol, 't')
-        
     def testSearchIndex(self):
         """
         @Status tests that an index record(s) can be searched
@@ -288,21 +320,21 @@ class TestSearch(unittest.TestCase):
         driver = self.driver
         form = self.form
         
-        #find the table field to check
+        #find the Index grid
         table_element = driver.find_element_by_id("indexGrid")
         table = Table(table_element)
-        #puts an X in the Prot-sxn by age 7 box
+        #puts an X in the Prot-wm by age 7 box
         cell = table.get_cell("prot-wm", "7")
         cell.click()
-        #puts an X in the Prot-sxn by age 8 box
+        #puts an X in the Prot-wm by age 8 box
         cell = table.get_cell("prot-wm", "8")
         cell.click()
         wait.forAngular(driver)
         form.click_search()#click the search button
-        
+        #find the search results table
         results_table = driver.find_element_by_id("resultsTable")
         table = Table(results_table)
-        
+        #Iterate and print the search results headers
         header_cells = table.get_header_cells()
         print iterate.getTextAsList(header_cells)
         
@@ -311,10 +343,8 @@ class TestSearch(unittest.TestCase):
         print iterate.getTextAsList(cells)
         #print column 1
         symbols_cells = table.get_column_cells('Marker')
-        print iterate.getTextAsList(symbols_cells)
-        cells = table.get_column_cells(1)
-        print iterate.getTextAsList(cells)
-        
+        symbols = iterate.getTextAsList(symbols_cells)
+        self.assertEqual(symbols, ['Marker','Adgre1', 'Cdh5', 'Cdx4', 'Eng', 'Fgf8', 'Foxa2', 'Gata6', 'Itga2b', 'Kdr', 'Kdr', 'Kdr', 'Kdr', 'Lrp2', 'Nanog', 'Pdgfra', 'Pecam1', 'Pou3f1', 'Ptprc', 'T', 'T', 'Tal1', 'Tek'])
             
     def testResultsTable(self):
         """
