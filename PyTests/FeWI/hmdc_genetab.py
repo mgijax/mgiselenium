a07c11b2c7212f24a20aae328a83aa0a646ad4dc
jeff campbell <jeff.campbell.jax.org>
2016-11-29 07:58:19 -0500

    update hmdc genetab test

--- PyTests/FeWI/hmdc_genetab.py
+++ PyTests/FeWI/hmdc_genetab.py
@@ -100,6 +100,38 @@ class Test(unittest.TestCase):
         self.assertEqual(gene5.text, 'Tg(Gata1*V205G)1Mym')
         self.assertEqual(gene6.text, 'Tg(HBB-Gata1)G4Phi')
         
+    def test_uniquegenes_tab_genes(self):
+        '''
+        @status this test verifies the correct genes are returned for this query, in particular genes with special characters in their symbol.
+        '''
+        my_select = self.driver.find_element_by_xpath("//select[starts-with(@id, 'field_0_')]")#identifies the select field and picks the gene symbols option
+        for option in my_select.find_elements_by_tag_name("option"):
+            if option.text == 'Gene Symbol(s) or ID(s)':
+                option.click()
+                break
+        
+        self.driver.find_element_by_name("formly_3_input_input_0").send_keys("Tg(IGH@*)SALed")#identifies the input field and enters gata1
+        wait.forAngular(self.driver)
+        self.driver.find_element_by_id("searchButton").click()
+        wait.forAngular(self.driver)
+        #identify the Genes tab and verify the tab's text
+        gene_tab = self.driver.find_element_by_css_selector("ul.nav.nav-tabs > li.uib-tab.nav-item.ng-scope.ng-isolate-scope:nth-child(2) > a.nav-link.ng-binding")
+        print gene_tab.text
+        self.assertEqual(gene_tab.text, "Genes (1)", "Genes tab is not visible!")
+        gene_tab.click()
+        
+        gene_table = Table(self.driver.find_element_by_id("geneTable"))
+        
+        cells = gene_table.get_column_cells("Gene Symbol")
+        
+        print iterate.getTextAsList(cells)
+        #displays each row of gene data
+        gene1 = cells[1]
+        
+        #asserts that the correct genes in the correct order are returned
+        self.assertEqual(gene1.text, 'Tg(IGH@*)SALed')
+                
+        
     def test_genes_tab_diseases(self):
         '''
         @status this test verifies the correct diseases are returned for this query.
