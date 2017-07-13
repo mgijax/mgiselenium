1ef5cb89637aef731cd7dc7e17e7f779094edfce
Jill Lewis <jill.lewis@jax.org>
2017-07-13 14:18:17 -0400

    Updates to HMDC tests to make them more tolerant of data changes

--- PyTests/FeWI/hmdc_diseasetab.py
+++ PyTests/FeWI/hmdc_diseasetab.py
@@ -1,7 +1,8 @@
 '''
 Created on Nov 15, 2016
-This test should check the column headers for correct display and order. It currently verifies the returned diseases and their order and the DO IDs in correct order, 
-tests for the other columns need to be added later.
+This test should check the column headers for correct display and order. It currently verifies the returned diseases and their order 
+and the DO IDs in correct order, tests for the other columns need to be added later.
+Updated: July 2017 (jlewis) - updates to make Disease Tab tests more tolerant to changes in annotations.  e.g. removing counts
 @author: jeffc
 '''
 import unittest
@@ -29,8 +30,9 @@ class TestDiseaseTab(unittest.TestCase):
         
     def test_diseases_tab_headers(self):
         '''
-        @status this test verifies the headings on the disease tab are correct and in the correct order.
-        @see HMDC-diesease-?
+        @status This test verifies the column headings on the Disease Tab are correct and in the correct order.  Updated July 2017 to be 
+                more tolerant of data changes by removing disease count.
+        @see HMDC-disease-1,2,3,4,5,7,8 (column headings)
         '''
         print ("BEGIN test_diseases_tab_headers")
         my_select = self.driver.find_element_by_xpath("//select[starts-with(@id, 'field_0_')]")#identifies the select field and picks the gene symbols option
@@ -39,14 +41,15 @@ class TestDiseaseTab(unittest.TestCase):
                 option.click()
                 break
         
-        self.driver.find_element_by_name("formly_3_input_input_0").send_keys("Gata1")#indentifies the input field and enters gata1
+        self.driver.find_element_by_name("formly_3_input_input_0").send_keys("Gata1")#identifies the input field and enters gata1
         wait.forAngular(self.driver)
         self.driver.find_element_by_id("searchButton").click()
-        #identify the Genes tab and verify the tab's text
+        
+        #identify the Disease Tab and click on it
         disease_tab = self.driver.find_element_by_css_selector("ul.nav.nav-tabs > li.uib-tab.nav-item.ng-scope.ng-isolate-scope:nth-child(3) > a.nav-link.ng-binding")
+        time.sleep(2)
         print disease_tab.text
         time.sleep(2)
-        self.assertEqual(disease_tab.text, "Diseases (3)", "Diseases tab is not visible!")
         disease_tab.click()
         disease_table_headers = self.driver.find_element_by_id("diseaseTable").find_element_by_css_selector("tr")
         items = disease_table_headers.find_elements_by_tag_name("th")
@@ -61,8 +64,9 @@ class TestDiseaseTab(unittest.TestCase):
         
     def test_diseases_tab_diseases(self):
         '''
-        @status this test verifies the correct diseases are returned for this query. This query uses search option Gene Symbol(s) or ID(s)
-        @see HMDC-GQ-??
+        @status This test verifies the correct diseases are returned for this query as-of July 2017. This query uses search option 
+                Gene Symbol(s) or ID(s).  Verified disease returned due to a Mouse, Human, and Mouse/Human annotations.
+        @see HMDC-GQ-1 (gene symbol query); HMDC-disease-9 (return diseases for a gene search)
         '''
         print ("BEGIN test_diseases_tab_diseases")
         my_select = self.driver.find_element_by_xpath("//select[starts-with(@id, 'field_0_')]")#identifies the select field and picks the gene symbols option
@@ -77,26 +81,27 @@ class TestDiseaseTab(unittest.TestCase):
         wait.forAngular(self.driver)
         #identify the Disease tab and verify the tab's text
         disease_tab = self.driver.find_element_by_css_selector("ul.nav.nav-tabs > li.uib-tab.nav-item.ng-scope.ng-isolate-scope:nth-child(3) > a.nav-link.ng-binding")
+        time.sleep(2)
         print disease_tab.text
         time.sleep(2)
-        self.assertEqual(disease_tab.text, "Diseases (3)", "Diseases tab is not visible!")
+        
         disease_tab.click()
         disease_table = Table(self.driver.find_element_by_id("diseaseTable"))
         cells = disease_table.get_column_cells("Disease")
         print iterate.getTextAsList(cells)
-        #displays each row of gene data
-        disease1 = cells[1]
-        disease2 = cells[2]
-        disease3 = cells[3]
-        #asserts that the correct genes in the correct order are returned
-        self.assertEqual(disease1.text, 'Down syndrome')
-        self.assertEqual(disease2.text, 'myelofibrosis')
-        self.assertEqual(disease3.text, 'thrombocytopenia')
+        
+        diseaseNamesReturned = iterate.getTextAsList(cells)
+        
+        #asserts that the following diseases are returned
+        self.assertIn('Down syndrome', diseaseNamesReturned) # Human GATA1 annotation
+        self.assertIn('myelofibrosis', diseaseNamesReturned) #  Mouse Gata1 annotation
+        self.assertIn('thrombocytopenia', diseaseNamesReturned) # Human and Mouse GATA1/Gata1 annotations
         
     def test_diseases_tab_diseases2(self):
         '''
-        @status this test verifies the correct diseases are returned for this query. This query uses search option Phenotype or Disease Name
-        @see HMDC-??-??
+        @status this test verifies the correct diseases are returned for this query. This query uses search option Phenotype or Disease Name for a 
+                query by an MP term name.
+        @see HMDC-PQ-1 (single token MP term); HMDC-disease-11 (return diseases due to association to genocluster returned)
         '''
         print ("BEGIN test_diseases_tab_diseases2")
         my_select = self.driver.find_element_by_xpath("//select[starts-with(@id, 'field_0_')]")#identifies the select field and picks the gene symbols option
@@ -109,53 +114,58 @@ class TestDiseaseTab(unittest.TestCase):
         wait.forAngular(self.driver)
         self.driver.find_element_by_id("searchButton").click()
         wait.forAngular(self.driver)
+        
         #identify the Genes tab and verify the tab's text
         disease_tab = self.driver.find_element_by_css_selector("ul.nav.nav-tabs > li.uib-tab.nav-item.ng-scope.ng-isolate-scope:nth-child(3) > a.nav-link.ng-binding")
+        time.sleep(2)
         print disease_tab.text
         time.sleep(2)
-        self.assertEqual(disease_tab.text, "Diseases (2)", "Diseases tab is not visible!")
+        
         disease_tab.click()
         disease_table = Table(self.driver.find_element_by_id("diseaseTable"))
         cells = disease_table.get_column_cells("Disease")
         print iterate.getTextAsList(cells)
-        #displays each row of gene data
-        disease1 = cells[1]
-        disease2 = cells[2]
+        
+        diseaseNamesReturned = iterate.getTextAsList(cells)
         #asserts that the correct genes in the correct order are returned
-        self.assertEqual(disease1.text, 'erythropoietic protoporphyria')
-        self.assertEqual(disease2.text, 'xeroderma pigmentosum group G')
+        self.assertIn('erythropoietic protoporphyria', diseaseNamesReturned)
+        self.assertIn('xeroderma pigmentosum group G', diseaseNamesReturned)
                 
         
     def test_diseases_tab_doids(self):
         '''
         @status this test verifies the correct DO IDs are returned for this query. This query uses search option Phenotype or Disease ID(s)
-        this ID  should bring back the disease Carney complex
-        @see HMDC-DQ-9
+        this ID  should bring back the disease "Carney complex".  Updated July 2017 to make more tolerant to new annotations.  Count removed and 
+        modified assertEqual to assertIn.
+        @see HMDC-DQ-9 (primary DOID query), HMDC-disease-10 (query by DOID, return diseases by DOID)
         '''
         print ("BEGIN test_diseases_tab_doids")
-        my_select = self.driver.find_element_by_xpath("//select[starts-with(@id, 'field_0_')]")#identifies the select field and picks the gene symbols option
+        
+        my_select = self.driver.find_element_by_xpath("//select[starts-with(@id, 'field_0_')]") #identifies the select field
         for option in my_select.find_elements_by_tag_name("option"):
             if option.text == 'Disease or Phenotype ID(s)':
                 option.click()
                 break
         
-        self.driver.find_element_by_name("formly_3_input_input_0").send_keys("DOID:0050471")#identifies the input field and enters gata1
+        self.driver.find_element_by_name("formly_3_input_input_0").send_keys("DOID:0050471") #identifies the input field and enters ID for "Carney complex"
         wait.forAngular(self.driver)
         self.driver.find_element_by_id("searchButton").click()
         wait.forAngular(self.driver)
-        #identify the Genes tab and verify the tab's text
+        
+        #identify the Disease Tab and click on it
         disease_tab = self.driver.find_element_by_css_selector("ul.nav.nav-tabs > li.uib-tab.nav-item.ng-scope.ng-isolate-scope:nth-child(3) > a.nav-link.ng-binding")
         time.sleep(2)
-        self.assertEqual(disease_tab.text, "Diseases (2)", "Diseases tab is not visible!")
+        print disease_tab.text
+        time.sleep(2)
+        
         disease_tab.click()
         disease_table = Table(self.driver.find_element_by_id("diseaseTable"))
         cells = disease_table.get_column_cells("DO ID")
         print iterate.getTextAsList(cells)
-        #displays each row of gene data
-        doid1 = cells[1]
-        #asserts that the correct genes in the correct order are returned
-        self.assertEqual(doid1.text, 'DOID:0050471')
-    
+        diseaseIdsReturned = iterate.getTextAsList(cells)
+        self.assertIn('DOID:0050471', diseaseIdsReturned) #verify DOID entered is returned in Disease Tab
+        
+       
     def tearDown(self):
         self.driver.close()
        
